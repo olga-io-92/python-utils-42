606c4a6fbea4dd13b1da41fb50056e58f2562efd
olga-io-92 <hanguyen16.081966@gmail.com>
2026-08-04 03:40:21 +0300

    Refactor consolidate constants for performance

---
+++ constants.py
@@ -0,0 +1,30 @@
+CONSTANTS = {
+    'TIMEOUT': 30,
+    'RETRY_COUNT': 5,
+    'API_URL': 'https://api.example.com/v1/',
+    'DEFAULT_LANGUAGE': 'en',
+    'MAX_CONNECTIONS': 10
+}
+
+HTTP_STATUS_CODES = {
+    200: 'OK',
+    404: 'Not Found',
+    500: 'Internal Server Error'
+}
+
+SUPPORTED_FORMATS = ['json', 'xml', 'csv']
+
+CACHE_TIMEOUT = 60
+
+LOGGING_LEVELS = {
+    'DEBUG': 10,
+    'INFO': 20,
+    'WARNING': 30,
+    'ERROR': 40,
+    'CRITICAL': 50
+}
+
+API_HEADERS = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}

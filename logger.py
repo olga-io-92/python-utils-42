86b7cbb5810bcfb2e5b8ccff6b272a22ab395aac
olga-io-92 <hanguyen16.081966@gmail.com>
2026-08-05 03:09:10 +0300

    ➕ logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,15 +1,14 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=10485760, backup_count=5):
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-
+def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
     handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-
     logger.addHandler(handler)
     return logger
 
-logger = setup_logger()
+logger = setup_logger()  
+logger.info('Logger is set up successfully.')

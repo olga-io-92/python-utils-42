fb65d01205c67342d001dcac4ab53623b49d04a7
olga-io-92 <hanguyen16.081966@gmail.com>
2026-08-07 03:11:33 +0300

    🔧 logging setup for application

--- logger.py
+++ logger.py
@@ -1,33 +1,25 @@
 import logging
 
-class CustomLogger:
-    def __init__(self, name):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
 
-    def debug(self, message):
-        self._log_with_error_handling(self.logger.debug, message)
+def setup_logger(name, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    if not logger.hasHandlers():
+        logger.addHandler(ch)
+    return logger
 
-    def info(self, message):
-        self._log_with_error_handling(self.logger.info, message)
 
-    def warning(self, message):
-        self._log_with_error_handling(self.logger.warning, message)
+def log_exception(logger, exception):
+    logger.exception('An error occurred: %s', exception)
 
-    def error(self, message):
-        self._log_with_error_handling(self.logger.error, message)
 
-    def critical(self, message):
-        self._log_with_error_handling(self.logger.critical, message)
+def log_message(logger, message):
+    logger.info(message)
 
-    def _log_with_error_handling(self, log_method, message):
-        try:
-            if not isinstance(message, str):
-                raise ValueError('Message must be a string')
-            log_method(message)
-        except Exception as e:
-            self.logger.error(f'Logging failed: {str(e)}')
+
+def log_warning(logger, message):
+    logger.warning(message)

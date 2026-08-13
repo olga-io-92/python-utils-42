9798aa5765eba3d618af514999b26d5cd3c01cf2
olga-io-92 <hanguyen16.081966@gmail.com>
2026-08-13 15:15:40 +0300

    Improve error handling for logger methods

--- logger.py
+++ logger.py
@@ -1,15 +1,40 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(name, log_file, level=logging.INFO):
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=5)
-    handler.setFormatter(formatter)
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.addHandler(handler)
-    return logger
+class Logger:
+    def __init__(self, name, level=logging.INFO):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def log_info(self, message):
+        self.logger.info(message)
+
+    def log_warning(self, message):
+        self.logger.warning(message)
+
+    def log_error(self, message):
+        self.logger.error(message)
+
+    def log_exception(self, message, exc_info=True):
+        self.logger.exception(message, exc_info=exc_info)
+
+    def log_debug(self, message):
+        self.logger.debug(message)
+
+    def log_critical(self, message):
+        self.logger.critical(message)  
 
 if __name__ == '__main__':
-    logger = setup_logger('my_logger', 'app.log')
-    logger.info('Logger setup complete.')
+    log = Logger(__name__)
+    log.log_info('This is an info log')
+    log.log_warning('This is a warning log')
+    log.log_error('This is an error log')
+    log.log_debug('This is a debug log')
+    log.log_critical('This is a critical log')
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        log.log_exception('Division by zero error occurred')

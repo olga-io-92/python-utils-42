2c12fb7586bae180f8a4f40f76fadaa7b4ed7f89
olga-io-92 <hanguyen16.081966@gmail.com>
2026-08-15 15:15:53 +0300

    Refactor configuration loader with defaults

--- config.py
+++ config.py
@@ -2,20 +2,31 @@ import json
 import os
 
 class ConfigLoader:
-    def __init__(self, default_config_path):
-        self.default_config_path = default_config_path
-        self.config = self.load_defaults()
+    def __init__(self, default_config=None):
+        self.default_config = default_config or {}
+        self.user_config = {}
 
-    def load_defaults(self):
-        with open(self.default_config_path, 'r') as file:
-            return json.load(file)
-
-    def update(self, updates):
-        self.config.update(updates)
+    def load(self, filepath):
+        if os.path.exists(filepath):
+            with open(filepath, 'r') as file:
+                self.user_config = json.load(file)
+        else:
+            self.user_config = {}
 
     def get(self, key, default=None):
-        return self.config.get(key, default)
+        return self.user_config.get(key, self.default_config.get(key, default))
 
+    def set(self, key, value):
+        self.user_config[key] = value
+
+    def save(self, filepath):
+        with open(filepath, 'w') as file:
+            json.dump(self.user_config, file, indent=4)
+
+# Example default configuration
 if __name__ == '__main__':
-    loader = ConfigLoader('default_config.json')
-    print(loader.get('some_key', 'default_value'))
+    defaults = {'host': 'localhost', 'port': 8080}
+    config_loader = ConfigLoader(defaults)
+    config_loader.load('config.json')
+    print(config_loader.get('host'))
+    print(config_loader.get('port'))
